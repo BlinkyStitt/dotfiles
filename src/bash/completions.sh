bd10ab4fdbcce978eab28626dd878b931f5177a1
Bryan Stitt <bryan@stitthappens.com>
2012-02-09 15:47:28 -0800

    tab completion for ssh

--- src/bash/completions.sh
+++ src/bash/completions.sh
@@ -11,3 +11,5 @@ done
 # TODO: write this in python
 complete -C ~/.bash/completion_scripts/project_completion.rb -o default c
 
+# Add tab completion for SSH hostnames based on ~/.ssh/config, ignoring wildcards
+[ -e "$HOME/.ssh/config" ] && complete -o "default" -o "nospace" -W "$(grep "^Host" ~/.ssh/config | grep -v "[?*]" | cut -d " " -f2)" scp sftp ssh
