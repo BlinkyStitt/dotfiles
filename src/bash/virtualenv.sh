2292c0d52fe4e5dd999e2654e4a8c9423c2d9ec3
Bryan Stitt <bryan@stitthappens.com>
2012-06-26 11:29:04 -0700

    source APPLICATION_DIR for flask-base environments

--- src/bash/virtualenv.sh
+++ src/bash/virtualenv.sh
@@ -2,7 +2,9 @@ if command_exists virtualenvwrapper.sh; then
     source `which virtualenvwrapper.sh`
 fi
 
-if [ "$VIRTUAL_ENV" ]; then 
-        source $VIRTUAL_ENV/bin/activate; 
+if [ "$APPLICATION_DIR" ]; then
+    source $APPLICATION_DIR/activate
+elif [ "$VIRTUAL_ENV" ]; then 
+    source $VIRTUAL_ENV/bin/activate; 
 fi 
 
