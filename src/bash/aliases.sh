b44508e30ee9f7cf3556abf778fd0fad1b1d028b
Bryan Stitt <bryan@uber.com>
2012-08-13 15:03:36 -0700

    split aliases between mac and linux

--- src/bash/aliases.sh
+++ src/bash/aliases.sh
@@ -1,6 +1,13 @@
 # Shows most used commands, cool script I got this from: http://lifehacker.com/software/how-to/turbocharge-your-terminal
 alias profileme="history | awk '{print \$2}' | awk 'BEGIN{FS=\"|\"}{print \$1}' | sort | uniq -c | sort -n | tail -n 20 | sort -nr"
 
-# ls
-# todo: different flags for mac and linux
-alias ls='ls -hF'
+if [[ `uname` == 'Darwin' ]]; then
+    alias ls='ls -hF'
+
+    # Get rid of those pesky .DS_Store files recursively
+    alias dsstore-clean='find . -type f -name .DS_Store -print0 | xargs -0 rm'
+
+    alias flushdns='dscacheutil -flushcache'
+else
+    alias ls="ls --color=auto"
+fi
