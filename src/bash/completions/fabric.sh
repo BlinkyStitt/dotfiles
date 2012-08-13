7ecc2568eb99ddc065fb38a8f0f14e212094b033
Bryan Stitt <bryan@uber.com>
2012-08-13 15:09:55 -0700

    even better fabric completion

--- src/bash/completions/fabric.sh
+++ src/bash/completions/fabric.sh
@@ -2,10 +2,14 @@ _fab_completion()
 {
     COMPREPLY=()
 
-    local cur tasks
-
-    tasks=$(fab --shortlist 2>/dev/null)
-    _get_comp_words_by_ref cur
-    COMPREPLY=( $(compgen -W "${tasks}" -- ${cur}) )
+    local cur
+    if [ -f "fabfile.py" ]; then
+        cur="${COMP_WORDS[COMP_CWORD]}"
+        COMPREPLY=( $(compgen -W "$(fab -F short -l)" -- ${cur}) )
+        return 0
+    else
+        # no fabfile.py found. Don't do anything.        
+        return 1
+    fi
 }
 complete -F _fab_completion fab
