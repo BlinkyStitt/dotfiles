bab793047149d48a271c0b7b26fbe89081eeff74
Bryan Stitt <bryan@stitthappens.com>
2012-08-20 00:15:04 -0700

    improve aliases

--- src/bash/aliases.sh
+++ src/bash/aliases.sh
@@ -1,13 +1,11 @@
 # Shows most used commands, cool script I got this from: http://lifehacker.com/software/how-to/turbocharge-your-terminal
 alias profileme="history | awk '{print \$2}' | awk 'BEGIN{FS=\"|\"}{print \$1}' | sort | uniq -c | sort -n | tail -n 20 | sort -nr"
 
-if [[ `uname` == 'Darwin' ]]; then
-    alias ls='ls -hF'
+alias ls='ls -hF'
 
+if [[ `uname` == 'Darwin' ]]; then
     # Get rid of those pesky .DS_Store files recursively
     alias dsstore-clean='find . -type f -name .DS_Store -print0 | xargs -0 rm'
 
     alias flushdns='dscacheutil -flushcache'
-else
-    alias ls="ls --color=auto"
 fi
