dbb0cfed2bdabf2a57efe6bbd3f2339d5c28d219
Bryan Stitt <bryan@uber.com>
2012-08-27 14:56:22 -0700

    set UBER_HOME from brew if possible

--- src/bash/uber.sh
+++ src/bash/uber.sh
@@ -1,4 +1,9 @@
-#export UBER_HOME="/Users/`whoami`/Uber"
-if [ -d "${HOME}/Uber" ]; then
-    export UBER_HOME="$HOME/Uber"
+if command_exists brew; then
+    if [ -d "`brew --prefix WyseNynja/uber/uber-home`" ]; then
+        export UBER_HOME=`brew --prefix WyseNynja/uber/uber-home`
+    elif [ -d "`brew --prefix uber/uber/uber-home`" ]; then
+        export UBER_HOME=`brew --prefix uber/uber/uber-home`
+    elif [ -d "${HOME}/Uber" ]; then
+        export UBER_HOME="$HOME/Uber"
+    fi
 fi
