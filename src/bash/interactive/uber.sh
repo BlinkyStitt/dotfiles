ca37446909e4eb9bad835cb858f1468838d71561
Bryan Stitt <bryan@uber.com>
2012-08-28 12:47:05 -0700

    new UBER_HOME repos

--- src/bash/interactive/uber.sh
+++ src/bash/interactive/uber.sh
@@ -1,8 +1,6 @@
 if command_exists brew; then
-    if [ -d "`brew --prefix WyseNynja/uber/uber-home`" ]; then
-        export UBER_HOME=`brew --prefix WyseNynja/uber/uber-home`
-    elif [ -d "`brew --prefix uber/uber/uber-home`" ]; then
-        export UBER_HOME=`brew --prefix uber/uber/uber-home`
+    if [ -d "`brew --prefix uber/alt/uber-home`" ]; then
+        export UBER_HOME=`brew --prefix uber/alt/uber-home`
     elif [ -d "${HOME}/Uber" ]; then
         export UBER_HOME="$HOME/Uber"
     fi
