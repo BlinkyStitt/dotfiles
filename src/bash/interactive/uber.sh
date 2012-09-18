ee2d0900659a12803caf0ad78139f6c429a70da0
Bryan Stitt <bryan@uber.com>
2012-09-18 13:36:59 -0700

    simplify UBER_HOME

--- src/bash/interactive/uber.sh
+++ src/bash/interactive/uber.sh
@@ -1,7 +1,3 @@
-if command_exists brew; then
-    if [ -d "`brew --prefix uber/alt/uber-home`" ]; then
-        export UBER_HOME=`brew --prefix uber/alt/uber-home`
-    elif [ -d "${HOME}/Uber" ]; then
-        export UBER_HOME="$HOME/Uber"
-    fi
+if [ -d "${HOME}/Uber" ]; then
+    export UBER_HOME="$HOME/Uber"
 fi
