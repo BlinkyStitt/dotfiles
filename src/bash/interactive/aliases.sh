93f42a0ea0d32675db51b91bc35b18620dac5436
Bryan Stitt <bryan@stitthappens.com>
2012-10-26 15:05:19 -0700

    improve ls colors on linux

--- src/bash/interactive/aliases.sh
+++ src/bash/interactive/aliases.sh
@@ -2,13 +2,16 @@
 # Shows most used commands, cool script I got this from: http://lifehacker.com/software/how-to/turbocharge-your-terminal
 alias profileme="history | awk '{print \$2}' | awk 'BEGIN{FS=\"|\"}{print \$1}' | sort | uniq -c | sort -n | tail -n 20 | sort -nr"
 
-alias ls='ls -hF'
 
 if shell_is_osx; then
     # Get rid of those pesky .DS_Store files recursively
     alias dsstore-clean='find . -type f -name .DS_Store -print0 | xargs -0 rm'
 
     alias flushdns='dscacheutil -flushcache'
+
+    alias ls='ls -hF'
+elif shell_is_linux; then
+    alias ls='ls -hF --color'
 fi
 
 # mkdir and cd there
