a1f8a156052c258758010e2263fc5bcff47f3975
Bryan Stitt <bryan@stitthappens.com>
2012-10-29 22:15:03 -0700

    make git prompt more robust

--- src/bash/interactive/prompt.sh
+++ src/bash/interactive/prompt.sh
@@ -26,31 +26,32 @@ export PS1_YELLOW="\[$COLOR_YELLOW\]"
 export PS1_BLUE="\[$COLOR_BLUE\]"
 
 read -d '' PROMPT_COMMAND <<"EOF"
-LAST_STATUS_CODE=$?;
+LAST_STATUS_CODE=$?
 if [[ $LAST_STATUS_CODE != 0 ]]; then
     STAT_COLOR=$PS1_LIGHT_RED
 else
     STAT_COLOR=$PS1_GREEN
-fi;
-STAT_MSG="$STAT_COLOR$LAST_STATUS_CODE$PS1_NC";
+fi
+STAT_MSG="$STAT_COLOR$LAST_STATUS_CODE$PS1_NC"
 if [ -n "$VIRTUAL_ENV" ]; then
 	VIRTUALENV_PS1="(`basename \"$VIRTUAL_ENV\"`) "
 else
 	VIRTUALENV_PS1=""
-fi;
-SHORTENED_PWD=`_dir_chomp "$(pwd)" 20`;
-_GIT_TMP=`__git_ps1`;
-if [[ $_GIT_TMP ]]; then
-    GIT_PS1="$PS1_YELLOW$_GIT_TMP";
-else
-    GIT_PS1="";
-fi;
+fi
+SHORTENED_PWD=`_dir_chomp "$(pwd)" 20`
+GIT_PS1=""
+if command_exists __git_ps1; then
+    _GIT_TMP=`__git_ps1`
+    if [ -n "$_GIT_TMP" ]; then
+        GIT_PS1="$PS1_YELLOW$_GIT_TMP"
+    fi
+fi
 if [ "$(id -u)" != "0" ]; then
     USER_COLOR="$PS1_GREEN"
     USER_PROMPT="\$"
 else
     COLOR_USER="$PS1_BLUE"
     USER_PROMPT="\#"
-fi;
+fi
 export PS1="$STAT_MSG $VIRTUALENV_PS1$USER_COLOR\\u@\\h $PS1_LIGHT_GREEN$SHORTENED_PWD$GIT_PS1$PS1_GREEN $PS1_NC$USER_PROMPT "
 EOF
