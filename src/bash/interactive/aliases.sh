e4b53dcbc53c4dbf5b980aa4218ff214c2f0065b
Bryan Stitt <bryan@uber.com>
2013-04-08 14:18:12 -0700

    add clusto aliases

--- src/bash/interactive/aliases.sh
+++ src/bash/interactive/aliases.sh
@@ -2,7 +2,6 @@
 # Shows most used commands, cool script I got this from: http://lifehacker.com/software/how-to/turbocharge-your-terminal
 alias profileme="history | awk '{print \$2}' | awk 'BEGIN{FS=\"|\"}{print \$1}' | sort | uniq -c | sort -n | tail -n 20 | sort -nr"
 
-
 if shell_is_osx; then
     # Get rid of those pesky .DS_Store files recursively
     alias dsstore-clean='find . -type f -name .DS_Store -print0 | xargs -0 rm'
@@ -16,3 +15,7 @@ fi
 
 # mkdir and cd there
 mkcd() { [ -n "$1" ] && mkdir -p "$@" && cd "$1"; }
+
+alias cinfo="ssh mgmt01-peak1.prod.uber.com clusto info"
+alias clp="ssh mgmt01-peak1.prod.uber.com clusto list-pool --recursive"
+
