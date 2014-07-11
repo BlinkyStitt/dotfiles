b92c887235ad170c37044086f05fc762c1694d2f
Bryan Stitt <bryan@uber.com>
2014-07-11 16:31:56 -0700

    Predictable SSH authentication socket location

--- src/bash/all/0_functions.sh
+++ src/bash/all/0_functions.sh
@@ -2,3 +2,11 @@
 
 # utility function to check if a command exists on the PATH
 command_exists() { type "$1" &> /dev/null; }
+
+# Predictable SSH authentication socket location.
+STATIC_SSH_AUTH_SOCK="$HOME/.ssh/ssh-auth-sock"
+if test $SSH_AUTH_SOCK && [ $SSH_AUTH_SOCK != $STATIC_SSH_AUTH_SOCK ]
+then
+    ln -sf $SSH_AUTH_SOCK $STATIC_SSH_AUTH_SOCK
+    export SSH_AUTH_SOCK=$STATIC_SSH_AUTH_SOCK
+fi
