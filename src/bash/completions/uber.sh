bd9af5f591c8102d4749eb103a62062378d06313
Bryan Stitt <bryan@uber.com>
2014-09-12 11:25:28 -0700

    add us alias

--- src/bash/completions/uber.sh
+++ src/bash/completions/uber.sh
@@ -22,3 +22,28 @@ if [[ -n ${UBER_HOME} && -d ${UBER_HOME} ]]; then
 
     complete -o default -o nospace -F _uber u;
 fi
+
+if [[ -n ${UBER_HOME} && -d "${UBER_HOME}/scripts/boxer/sync" ]]; then
+    # cd into UBER_HOME/scripts/boxer/sync
+    # todo: this is copy/pasted from ./code.sh, don't do that!
+    us() {
+        if [ -z $1 ]; then
+            cd ${UBER_HOME}/scripts/boxer/sync
+            return
+        fi
+
+        if [ ! -d ${UBER_HOME}/scripts/boxer/sync/$1 ]; then
+            echo "$1: No such project"
+            return
+        fi
+
+        cd ${UBER_HOME}/scripts/boxer/sync/$1
+        echo "cd `pwd`"
+    }
+    _ubersync() {
+        COMPREPLY=($(compgen -W '$(ls ${UBER_HOME}/)' -- ${COMP_WORDS[COMP_CWORD]}))
+        return 0
+    }
+
+    complete -o default -o nospace -F _ubersync us;
+fi
