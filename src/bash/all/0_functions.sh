2ef1ce2d42cfb10c4215ac100a7431f24d3fd092
Bryan Stitt <bryan@uber.com>
2014-09-12 11:26:46 -0700

    ssh-reagent

--- src/bash/all/0_functions.sh
+++ src/bash/all/0_functions.sh
@@ -4,9 +4,21 @@
 command_exists() { type "$1" &> /dev/null; }
 
 # Predictable SSH authentication socket location.
+# todo: this does not work right if you `ssh localhost`
 STATIC_SSH_AUTH_SOCK="$HOME/.ssh/ssh-auth-sock"
-if test $SSH_AUTH_SOCK && [ $SSH_AUTH_SOCK != $STATIC_SSH_AUTH_SOCK ]
-then
+if test $SSH_AUTH_SOCK && [ $SSH_AUTH_SOCK != $STATIC_SSH_AUTH_SOCK ]; then
     ln -sf $SSH_AUTH_SOCK $STATIC_SSH_AUTH_SOCK
     export SSH_AUTH_SOCK=$STATIC_SSH_AUTH_SOCK
 fi
+
+ssh-reagent () {
+    for agent in $SSH_AUTH_SOCK $STATIC_SSH_AUTH_SOCK /tmp/ssh-*/agent.*; do
+        export SSH_AUTH_SOCK=$agent
+        if ssh-add -l 2>&1 > /dev/null; then
+            echo "Found working SSH Agent: ${SSH_AUTH_SOCK}"
+            ssh-add -l
+            return
+        fi
+    done
+    echo "Cannot find ssh agent - maybe you should reconnect and forward it?"
+}
