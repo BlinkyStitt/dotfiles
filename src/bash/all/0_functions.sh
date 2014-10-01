49c6ffc9c8ce2f5406e6c1c74fa5810b155ba0f0
Bryan Stitt <bryan@uber.com>
2014-10-01 16:54:06 -0700

    disable busted ssh agent stuff

--- src/bash/all/0_functions.sh
+++ src/bash/all/0_functions.sh
@@ -5,11 +5,11 @@ command_exists() { type "$1" &> /dev/null; }
 
 # Predictable SSH authentication socket location.
 # todo: this does not work right if you `ssh localhost`
-STATIC_SSH_AUTH_SOCK="$HOME/.ssh/ssh-auth-sock"
-if test $SSH_AUTH_SOCK && [ $SSH_AUTH_SOCK != $STATIC_SSH_AUTH_SOCK ]; then
-    ln -sf $SSH_AUTH_SOCK $STATIC_SSH_AUTH_SOCK
-    export SSH_AUTH_SOCK=$STATIC_SSH_AUTH_SOCK
-fi
+#STATIC_SSH_AUTH_SOCK="$HOME/.ssh/ssh-auth-sock"
+#if test $SSH_AUTH_SOCK && [ $SSH_AUTH_SOCK != $STATIC_SSH_AUTH_SOCK ]; then
+#    ln -sf $SSH_AUTH_SOCK $STATIC_SSH_AUTH_SOCK
+#    export SSH_AUTH_SOCK=$STATIC_SSH_AUTH_SOCK
+#fi
 
 ssh-reagent () {
     for agent in $SSH_AUTH_SOCK $STATIC_SSH_AUTH_SOCK /tmp/ssh-*/agent.*; do
