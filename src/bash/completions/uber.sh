daea162622eb1dfa501df6991cdeb46871fd2f42
Bryan Stitt <bryan@uber.com>
2014-10-10 14:32:13 -0700

    move us to new sync dir

--- src/bash/completions/uber.sh
+++ src/bash/completions/uber.sh
@@ -23,21 +23,21 @@ if [[ -n ${UBER_HOME} && -d ${UBER_HOME} ]]; then
     complete -o default -o nospace -F _uber u;
 fi
 
-if [[ -n ${UBER_HOME} && -d "${UBER_HOME}/scripts/boxer/sync" ]]; then
-    # cd into UBER_HOME/scripts/boxer/sync
+if [[ -n ${UBER_HOME} && -d "${UBER_HOME}/sync" ]]; then
+    # cd into UBER_HOME/sync
     # todo: this is copy/pasted from ./code.sh, don't do that!
     us() {
         if [ -z $1 ]; then
-            cd ${UBER_HOME}/scripts/boxer/sync
+            cd ${UBER_HOME}/sync
             return
         fi
 
-        if [ ! -d ${UBER_HOME}/scripts/boxer/sync/$1 ]; then
+        if [ ! -d ${UBER_HOME}/sync/$1 ]; then
             echo "$1: No such project"
             return
         fi
 
-        cd ${UBER_HOME}/scripts/boxer/sync/$1
+        cd ${UBER_HOME}/sync/$1
         echo "cd `pwd`"
     }
     _ubersync() {
