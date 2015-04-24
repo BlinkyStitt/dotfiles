01ee5c6661c4064ef0399b472e0e8496010286ac
Bryan Stitt <bryan@stitthappens.com>
2015-04-23 21:39:37 -0700

    gpg-agent

--- src/bash/interactive/gpg_agent.sh
+++ src/bash/interactive/gpg_agent.sh
@@ -1,10 +1,10 @@
-# todo: make this easy to opt-into
-
-#envfile="$HOME/.gnupg/gpg-agent.env"
-#if [[ -e "$envfile" ]] && kill -0 $(grep GPG_AGENT_INFO "$envfile" | cut -d: -f 2) 2>/dev/null; then
-#    eval "$(cat "$envfile")"
-#else
-#    eval "$(gpg-agent --daemon --enable-ssh-support --write-env-file "$envfile")"
-#fi
-#export GPG_AGENT_INFO
-#export SSH_AUTH_SOCK
+if [[ -e "$HOME/.gnupg/.override-ssh-agent" ]]; then
+    envfile="$HOME/.gnupg/gpg-agent.env"
+    if [[ -e "$envfile" ]] && kill -0 $(grep GPG_AGENT_INFO "$envfile" | cut -d: -f 2) 2>/dev/null; then
+        eval "$(cat "$envfile")"
+    else
+        eval "$(gpg-agent --daemon --enable-ssh-support --write-env-file "$envfile")"
+    fi
+    export GPG_AGENT_INFO
+    export SSH_AUTH_SOCK
+fi
