5cd038a73c527caf594d6b3cff26851e844f049e
Bryan Stitt <bryan@stitthappens.com>
2016-05-10 19:28:26 -0700

    fortune is dumb

--- src/bash/login/hello.sh
+++ src/bash/login/hello.sh
@@ -3,13 +3,14 @@ if shell_is_interactive; then
     echo -e "${COLOR_BROWN}Kernel Information: " `uname -smr`
     echo -ne "${COLOR_GRAY}Uptime: "; uptime
     echo -ne "Server time is: "; date
-    echo -e "${COLOR_NC}Don't forget to update your dotfiles!"
 
     if command_exists fortune; then
         if command_exists cowsay; then
-            fortune -s | cowsay -s -W 76
+            cmd="cowsay -s -W 76"
         else
-            fortune -s
+            cmd=tee
         fi
+        echo "Don't forget to update your dotfiles! 'cd ~/.dotfiles; git pull'" | $cmd
     fi
+    echo -ne "${COLOR_NC}"
 fi
