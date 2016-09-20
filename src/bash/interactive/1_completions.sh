8d672cc27fe477ac10f56327742f716427337bf0
Bryan Stitt <bryan@stitthappens.com>
2016-09-19 23:55:28 -0700

    more completions

--- src/bash/interactive/1_completions.sh
+++ src/bash/interactive/1_completions.sh
@@ -1,22 +1,22 @@
 # run completions last since they may need config variables
 
 for i in ~/.bash/completions/*.sh ; do
-    if [ -r "$i" ]; then
-        if [ "$PS1" ]; then
-            . $i
-        else
-            . $i >/dev/null 2>&1
-        fi
+  if [ -r "$i" ]; then
+    if [ "$PS1" ]; then
+      . $i
+    else
+      . $i >/dev/null 2>&1
     fi
+  fi
 done
 
 # homebrew completion
 if command_exists brew; then
-  if [ -r `brew --prefix`/etc/bash_completion ]; then
-    . `brew --prefix`/etc/bash_completion
+  if [ -r "$(brew --prefix)/etc/bash_completion" ]; then
+    . "$(brew --prefix)/etc/bash_completion"
   fi
-else
-  if [ -f /usr/local/share/bash-completion/bash_completion.sh ]; then
-    . /usr/local/share/bash-completion/bash_completion.sh
-  fi
+fi
+
+if command_exists thefuck; then
+  eval "$(thefuck --alias)"
 fi
