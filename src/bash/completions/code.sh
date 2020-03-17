e068b245cafcb3c04f31c5fa3594af3898defa09
Bryan Stitt <bryan@stitthappens.com>
2020-03-17 13:49:04 -0700

    ski helper

--- src/bash/completions/code.sh
+++ src/bash/completions/code.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# i'm sure this could be better. but this is a quick way to get into my code directories with simple tab completion
 
 if [[ -n ${CODE_HOME} && -d ${CODE_HOME} ]]; then
     # cd into the code directory
@@ -25,4 +26,56 @@ if [[ -n ${CODE_HOME} && -d ${CODE_HOME} ]]; then
     }
 
     complete -o default -o nospace -F _c c
+
+    if [[ -d "${CODE_HOME}/satoshi_and_kin" ]]; then
+        # cd into the code/satoshi_and_kin directory
+        # todo: make this work deeper than one level
+        ski() {
+            local d=${1%@}
+
+            if [ -z "$d" ]; then
+                cd "${CODE_HOME}/satoshi_and_kin" || exit 1
+                return
+            fi
+
+            if [ ! -d "${CODE_HOME}/satoshi_and_kin/${d}" ]; then
+                echo "$1: No such project"
+                return
+            fi
+
+            cd "${CODE_HOME}/satoshi_and_kin/${d}" || exit 1
+            echo "cd $(pwd)"
+        }
+        _ski() {
+            COMPREPLY=($(compgen -W '$(ls ${CODE_HOME}/satoshi_and_kin/)' -- ${COMP_WORDS[COMP_CWORD]}))
+            return 0
+        }
+
+        complete -o default -o nospace -F _ski ski
+    elif [[ -d "${CODE_HOME}/ski" ]]; then
+        # cd into the code/ski directory
+        # todo: make this work deeper than one level
+        ski() {
+            local d=${1%@}
+
+            if [ -z "$d" ]; then
+                cd "${CODE_HOME}/ski" || exit 1
+                return
+            fi
+
+            if [ ! -d "${CODE_HOME}/ski/${d}" ]; then
+                echo "$1: No such project"
+                return
+            fi
+
+            cd "${CODE_HOME}/${d}" || exit 1
+            echo "cd $(pwd)"
+        }
+        _ski() {
+            COMPREPLY=($(compgen -W '$(ls ${CODE_HOME}/ski/)' -- ${COMP_WORDS[COMP_CWORD]}))
+            return 0
+        }
+
+        complete -o default -o nospace -F _ski ski
+    fi
 fi
